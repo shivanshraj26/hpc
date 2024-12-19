e4c661fcebf6f59481b54546b222dd681f5ad9c3
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-12-19 11:24:17 +0530

    Update p6.cpp

--- p6.cpp
+++ p6.cpp
@@ -10,9 +10,11 @@ int main(int argc, char **argv)
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
     srand((unsigned int)time(0) + world_rank); // or, srand(static_cast<unsigned>(time(0)) + world_rank);
     int mangoes_picked = rand() % 101;
     cout << "Robot " << world_rank << " picked " << mangoes_picked << " mangoes." << endl;
+    
     int total_mangoes;
     MPI_Reduce(&mangoes_picked, &total_mangoes, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     if (world_rank == 0)
